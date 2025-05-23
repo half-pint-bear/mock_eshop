6f11cc8f45582fea365119c93bf24546dc50e407
CrankyBear <jeremie.bozant@ipilyon.net>
2025-05-23 15:38:52 +0200

    feat: set up apiClient & home skeleton

--- src/app/routes.jsx
+++ src/app/routes.jsx
@@ -1,6 +1,6 @@
 import {createBrowserRouter} from "react-router-dom";
 import MainLayout from "../layouts/MainLayout.jsx";
-import HomePage from "../pages/HomePage.jsx";
+import HomePage from "../pages/homepage/HomePage.jsx";
 
 export const Routes = createBrowserRouter([
     {
---
+++ src/services/apiClient.jsx
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+    baseURL: 'https://dummyjson.com',
+    timeout: 10000,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+// Error interceptor
+apiClient.interceptors.response.use(
+    response => response,
+    error => {
+        console.error('API error:', error);
+        return Promise.reject(error);
+    }
+);
+
+// Basic CRUD functions
+export const apiClientGet = (endpoint, params = {}) =>
+    apiClient.get(endpoint, { params });
+
+export const apiClientPost = (endpoint, data = {}) =>
+    apiClient.post(endpoint, data);
+
+export const apiClientPut = (endpoint, data = {}) =>
+    apiClient.put(endpoint, data);
+
+export const apiClientDelete = (endpoint) =>
+    apiClient.delete(endpoint);
