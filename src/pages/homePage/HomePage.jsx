2347360298bbfd3be581f13104c6d778c38851b7
CrankyBear <jeremie.bozant@ipilyon.net>
2025-05-30 15:03:32 +0200

    feat: make home categorygrid full page

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -14,7 +14,6 @@ export default function HomePage() {
     }
     return (
         <div className={styles.container}>
-            <h1 className={styles.homePageTitle}>Home</h1>
             <div className={styles.row}>
                 <div className={styles.categoryGrid}>
                     <CategoryGrid categories={categories} />
--- src/pages/singleCategoryPage/components/ProductCard.jsx
+++ src/pages/singleCategoryPage/components/ProductCard.jsx
@@ -1,4 +1,4 @@
-import styles from "../styles/SingleCategoryPage.module.css";
+import styles from "../styles/ProductCard.module.css";
 import StarRating from "../../../shared/components/stars/StarRating.jsx";
 import Button from "../../../shared/components/buttons/Button.jsx";
 import btnStyles from "../../../shared/components/buttons/Button.module.css";
@@ -6,13 +6,14 @@ import useAverageRating from "../../../shared/hooks/useAverageRating.jsx";
 
 export default function ProductCard({ product }) {
     const avgRating = useAverageRating(product.reviews);
-console.log(avgRating);
+
     return (
         <li key={product.id} className={styles.li}>
             <img src={product.images[0]} className={styles.img} alt={product.title} />
             <h2>{product.title}</h2>
             <StarRating rating={avgRating} />
             <p>{product.price}</p>
+            <p>{product.meta.updatedAt}</p>
             <Button className={btnStyles.detailsBtn} to={`/products/${product.id}`}>Acheter</Button>
         </li>
     )
