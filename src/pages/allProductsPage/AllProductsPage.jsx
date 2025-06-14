4c11f9354c21818ea4b8e53d5c90d3cda967b0e8
CrankyBear <jeremie.bozant@ipilyon.net>
2025-06-14 21:15:43 +0200

    chore(ui): enhance pagination

--- src/pages/allProductsPage/AllProductsPage.jsx
+++ src/pages/allProductsPage/AllProductsPage.jsx
@@ -6,7 +6,7 @@ import styles from "./styles/AllProductsPage.module.css";
 import CategoryFilter from "./components/CategoryFilter.jsx";
 import useAllCategories from "../allCategoriesPage/hooks/useAllCategories.jsx";
 import ModalFilter from "./components/ModalFilter.jsx";
-import { SlidersHorizontal} from "lucide-react";
+import { SlidersHorizontal, ChevronsLeft, ChevronLeft, ChevronsRight, ChevronRight } from "lucide-react";
 
 export default function AllProductsPage() {
     const [page, setPage] = useState(1);
@@ -20,6 +20,21 @@ export default function AllProductsPage() {
     const closeModal = () => {
         setModalState("exiting");
         setTimeout(() => setModalState("closed"), 200);
+    }
+
+    const goToPage = (target) => setPage(Math.min(Math.max(1, target), totalPages));
+
+    //Surround current page number with -2/+2 closest
+    const generatePageNumbers = () => {
+        const range = [];
+        const min = Math.max(1, page - 2);
+        const max = Math.min(totalPages, page + 2);
+
+        for(let i = min; i < max; i++) {
+            range.push(i);
+        }
+
+        return range;
     }
 
     if (loading || loadingCategories) return <Loader />;
@@ -72,18 +87,28 @@ export default function AllProductsPage() {
                     <ProductGrid products={products} />
 
                     <div className={styles.pagination}>
-                        <button
-                            onClick={() => setPage(prev => Math.max(1, prev - 1))}
-                            disabled={page === 1}
-                        >
-                            Précédent
+                        <button onClick={() => goToPage(1)} disabled={page === 1}>
+                            <ChevronsLeft size={20} />
                         </button>
-                        <span>Page {page} sur {totalPages}</span>
-                        <button
-                            onClick={() => setPage(prev => Math.min(totalPages, prev + 1))}
-                            disabled={page === totalPages}
-                        >
-                            Suivant
+                        <button onClick={() => goToPage(page - 1)} disabled={page === 1}>
+                            <ChevronLeft size={20} />
+                        </button>
+
+                        {generatePageNumbers().map(num => (
+                            <button
+                                key={num}
+                                onClick={() => goToPage(num)}
+                                className={page === num ? styles.activePage : ""}
+                            >
+                                {num}
+                            </button>
+                        ))}
+
+                        <button onClick={() => goToPage(page + 1)} disabled={page === totalPages}>
+                            <ChevronRight size={20} />
+                        </button>
+                        <button onClick={() => goToPage(totalPages)} disabled={page === totalPages}>
+                            <ChevronsRight size={20} />
                         </button>
                     </div>
                 </main>
